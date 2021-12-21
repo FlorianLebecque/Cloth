2b542c872237a960dce0a55d25856c193a31da75
florian lebecque <florianlebecque@hotmail.com>
2021-12-21 12:00:09 +0100

    Corrected bunch of crap

--- classes/gpu/Gpu.cs
+++ classes/gpu/Gpu.cs
@@ -3,39 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using OpenTK.Compute.OpenCL;
 
-namespace Cloth.classes {
+
+namespace OPENCL {
     public class GPU {
         
-        Event event0;
-        ErrorCode err;
-        Device Gpu_1;
-        Context Gpu_context;
-        CommandQueue commandQueue;
+        CLEvent event0;
+        CLDevice Gpu_1;
+        CLContext Gpu_context;
+        CLCommandQueue commandQueue;
         
 
         public GPU(){
-            err = new ErrorCode();
-
             Gpu_1 = GetGpu();
             Gpu_context = GetContext(Gpu_1);
             commandQueue = InitCommandQueue(Gpu_context,Gpu_1);
         }
 
-        private Device GetGpu(){
-            ErrorCode errorCode;
-            Platform[] platforms = Cl.GetPlatformIDs(out errorCode);
-            var Lst_Devices = new List<Device>();
+        private CLDevice GetGpu(){
+            //ErrorCode errorCode;
+            CLPlatform[] platforms;
+            CL.GetPlatformIds(out platforms);
+            var Lst_Devices = new List<CLDevice>();
 
-            foreach (Platform platform in platforms)
-            {
-                String Device_Name = Cl.GetPlatformInfo(platform, PlatformInfo.Name, out errorCode).ToString();
-
-                //GUI : List box du formulaire au besoin
-                //Lsb_Devices_Names.Items.Add(Device_Name);
-
+            foreach (CLPlatform platform in platforms) {
+                CLDevice[] list;
+                CL.GetDeviceIds(platform, DeviceType.Gpu,out list);
                 //GPU uniquement 
-                foreach (Device device in Cl.GetDeviceIDs(platform, DeviceType.Gpu, out errorCode))
+                foreach (CLDevice device in list)
                 {
                     Lst_Devices.Add(device);
                 }
@@ -49,110 +45,140 @@ namespace Cloth.classes {
             return Lst_Devices[0];
         }
 
-        private Context GetContext(Device Gpu_1){
-            OpenCL.Net.ErrorCode errorCode;
-
-            Context Gpu_context = Cl.CreateContext(null, 1, new Device[] { Gpu_1 }, null, IntPtr.Zero, out errorCode);
-            if (errorCode != OpenCL.Net.ErrorCode.Success) {
+        private CLContext GetContext(CLDevice Gpu_1){
+            CLResultCode errorCode;
+            CLContext Gpu_context = CL.CreateContext((IntPtr)null, 1, new CLDevice[] { Gpu_1 }, (IntPtr)null, IntPtr.Zero, out errorCode);
+            if (errorCode != CLResultCode.Success) {
                 throw new Exception("Impossible de créer le contexte");
             }
 
             return Gpu_context;
         }
 
-        private CommandQueue InitCommandQueue(Context Gpu_context,Device Gpu_1){
-            OpenCL.Net.ErrorCode errorCode;
-            CommandQueue commandQueue = Cl.CreateCommandQueue(Gpu_context, Gpu_1, CommandQueueProperties.OutOfOrderExecModeEnable, out errorCode);
-            if (errorCode != OpenCL.Net.ErrorCode.Success) {
+        private CLCommandQueue InitCommandQueue(CLContext Gpu_context,CLDevice Gpu_1){
+            CLResultCode errorCode;
+            CLCommandQueue commandQueue = CL.CreateCommandQueue(Gpu_context, Gpu_1, (CommandQueueProperty)0, out errorCode);
+            if (errorCode != CLResultCode.Success) {
                 throw new Exception("Impossible de créer la liste de traitement");
             }
 
             return commandQueue;
         }
     
-        public IMem CreateBuffer(MemFlags mFlag, IntPtr size){
-            OpenCL.Net.ErrorCode err = new OpenCL.Net.ErrorCode();
+        public CLBuffer CreateBuffer<T>(MemoryFlags mFlag, T[] array) where T:unmanaged {
+            CLResultCode err = new CLResultCode();
 
-            IMem temp = Cl.CreateBuffer(Gpu_context, mFlag, size, out err);
+            int sizes = Marshal.SizeOf(typeof(T))*array.Count();
+            UIntPtr size = (UIntPtr)sizes;
 
-            if (err != ErrorCode.Success) {
+
+            CLBuffer t = CL.CreateBuffer(Gpu_context,MemoryFlags.ReadOnly,size,(IntPtr)null,out err);
+            //CLBuffer t = CL.CreateBuffer<T>(Gpu_context,mFlag,array,out err);
+
+            if (err != CLResultCode.Success) {
                 throw new Exception(String.Format("Unable to load memory: {0}", err.ToString()));                
             }
 
-            return temp;
+            return  t;
         }
     
-        private OpenCL.Net.Program CreateProgram(String path){
+        private CLProgram CreateProgram(String path){
             return GetProgram(path,Gpu_context,Gpu_1);
         }
 
-        private Program GetProgram(String path,Context Gpu_context,Device Gpu_1){
+        private CLProgram GetProgram(String path,CLContext Gpu_context,CLDevice Gpu_1){
             String Program_Source_Code = File.ReadAllText(path);
 
-            OpenCL.Net.ErrorCode err;
+            CLResultCode err;
 
             //Création du programme 
-            OpenCL.Net.Program program = Cl.CreateProgramWithSource(Gpu_context, 1, new[] { Program_Source_Code }, new[] {(IntPtr)Program_Source_Code.Length}, out err);
+            CLProgram program = CL.CreateProgramWithSource(Gpu_context,Program_Source_Code,out err);
             
-            Cl.BuildProgram(program, 1, new[] {Gpu_1}, "-cl-std=CL1.2", null, IntPtr.Zero);
+            CL.BuildProgram(program, 1, new[] {Gpu_1}, null, (IntPtr)null, (IntPtr)null);
+
+            byte[] result;
+            err = CL.GetProgramBuildInfo(program,Gpu_1,ProgramBuildInfo.Status,out result);
 
             //Récupérations des informations du build
-            if (Cl.GetProgramBuildInfo(program, Gpu_1, ProgramBuildInfo.Status, out err).CastTo<BuildStatus>() != BuildStatus.Success) {
+            if (result is not null) {
                 //Affichage si erreurs durant la compilation 
-                if (err != OpenCL.Net.ErrorCode.Success) {
+                if (err != CLResultCode.Success) {
                     String Output = "";
-                    Output += String.Format("ERROR: " + "Cl.GetProgramBuildInfo" + " (" + err.ToString() + ")");
-                    Output += String.Format("Cl.GetProgramBuildInfo != Success");
-                    Output += Cl.GetProgramBuildInfo(program, Gpu_1, ProgramBuildInfo.Log, out err);
+                    Output += String.Format("ERROR: " + "CL.GetProgramBuildInfo" + " (" + err.ToString() + ")");
+                    Output += String.Format("CL.GetProgramBuildInfo != Success");
                 }
             }
 
             return program;
         }
 
-        public Kernel CreateKernel(String path,string name){
-            OpenCL.Net.ErrorCode err = new OpenCL.Net.ErrorCode();
+        public CLKernel CreateKernel(String path,string name){
+            CLResultCode err = new CLResultCode();
 
-            Kernel kernel = Cl.CreateKernel(CreateProgram(path),name,out err);
-            if (err != ErrorCode.Success) {
+            CLKernel kernel = CL.CreateKernel(CreateProgram(path),name,out err);
+            if (err != CLResultCode.Success) {
                 throw new Exception(err.ToString());
             }
 
             return kernel;
         }
 
-        public void Upload(IMem buffer,IntPtr size,object data){
-            Cl.EnqueueWriteBuffer(
+        public unsafe void Upload<T>(CLBuffer buffer,T[] data) where T:unmanaged{
+
+            int sizes = Marshal.SizeOf(typeof(T))*data.Count();
+            UIntPtr size = new UIntPtr((uint)sizes);
+
+            //CL.EnqueueWriteBuffer(commandQueue,buffer,true,UIntPtr.Zero,size,new IntPtr(data.to),null,out event0);
+
+            CL.EnqueueWriteBuffer<T>(
                 commandQueue,
                 buffer,
-                Bool.True,
-                IntPtr.Zero,
-                size,
+                true,
+                UIntPtr.Zero,
                 data,
-                0,
                 null,
                 out event0
             );
 
         }
 
-        public void SetKernelArg(Kernel kernel,uint index,IMem buffer){
-            Cl.SetKernelArg(kernel, index, (IntPtr)Marshal.SizeOf(typeof(IntPtr)), buffer);
+        public unsafe void SetKernelArg(CLKernel kernel,uint index,CLBuffer buffer){
+            
+            //MemoryObjectType.Buffer; //4336
+            //MemoryObjectInfo.Size; //4354
+
+            byte[] result;
+            CL.GetMemObjectInfo(buffer,MemoryObjectInfo.Size,out result);
+            UInt32 size = BitConverter.ToUInt32(result);
+            UInt32 size2 = (UInt32)Marshal.SizeOf(typeof(CLBuffer));
+            UInt32 size3 = (UInt32)Marshal.SizeOf(typeof(IntPtr));
+
+            CLResultCode err = CL.SetKernelArg(kernel, index,(UIntPtr)Marshal.SizeOf(typeof(CLBuffer)), new IntPtr(&buffer));
+
+            if (err != CLResultCode.Success) {
+                throw new Exception(err.ToString());
+            }
         }
 
-        public void Execute(Kernel kernel,uint dim,int count){
-            IntPtr[] global = new IntPtr[1];
-            global[0] = new IntPtr(count);
-            
-            IntPtr[] local = new IntPtr[1];
-            int local_work = count;
-            if(local_work == 0){
-                local_work = 1;
-            }
-            local[0] = new IntPtr(local_work);
-            
+        private int GetMAXGROUPESIZE(){
+            byte[] MAX_WORK;
+            CL.GetDeviceInfo(Gpu_1,DeviceInfo.MaximumWorkGroupSize,out MAX_WORK);
+            return BitConverter.ToInt32(MAX_WORK);
+        }
+        public unsafe void Execute(CLKernel kernel,uint dim,int count){
+            UIntPtr[] global = new UIntPtr[dim];
+            global[0] = new UIntPtr((uint)count);
 
-            err = Cl.EnqueueNDRangeKernel(
+            
+            
+            UIntPtr[] local = new UIntPtr[1];
+            int local_work = count;
+
+            local[0] = new UIntPtr((uint)count);
+            
+            
+            CLResultCode err;
+            err = CL.EnqueueNDRangeKernel(
                 commandQueue,
                 kernel,
                 dim,
@@ -161,20 +187,18 @@ namespace Cloth.classes {
                 local,
                 0,
                 null,
-                out Event _
+                out event0
             );
 
-            if (err != ErrorCode.Success) {
+            if (err != CLResultCode.Success) {
                 throw new Exception(err.ToString());
             }
 
-            Cl.Finish(commandQueue);
-
-
+            CL.Finish(commandQueue);
         }
 
-        public void Download(IMem buffer,IntPtr size,object ouput){
-            Cl.EnqueueReadBuffer(commandQueue, (IMem)buffer, Bool.True, IntPtr.Zero, size, ouput, 0, null, out event0);
+        public void Download<T>(CLBuffer buffer,T[] ouput) where T:unmanaged{
+            CL.EnqueueReadBuffer(commandQueue, buffer, true, UIntPtr.Zero, ouput, null, out event0);
         }
         
         public void End(){

d9e5492c22237278f2079d1190b225861b2ba3ab
Florian Lebecque <florianlebecque@hotmail.com>
2025-10-08 18:42:43 +0200

    Fix shader path

--- utils/PlanetModelGenerator.cs
+++ utils/PlanetModelGenerator.cs
@@ -10,29 +10,33 @@ using static Raylib_cs.ShaderLocationIndex;
 using RaylibUtils;
 
 
-namespace Gravity.utils {
-    class PlanetModelGenerator {
+namespace Gravity.utils
+{
+    class PlanetModelGenerator
+    {
 
         public static Shader sh;
         public static bool ShaderInit = false;
-        public static unsafe Tuple<Model,Shader> getSphere(float radius) {
+        public static unsafe Tuple<Model, Shader> getSphere(float radius)
+        {
 
             Model md = LoadModelFromMesh(GenMeshSphere(radius, 32, 32));
 
-            if(!ShaderInit){
-                sh = LoadShader("resources\\shaders\\base.vs", "resources\\shaders\\base.fs");
-                
-                
-                Utils.SetShaderLocation(ref sh,SHADER_LOC_MATRIX_MODEL,"matModel");
-                
+            if (!ShaderInit)
+            {
+                sh = LoadShader("resources/shaders/base_lighting.vs", "resources/shaders/lighting.fs");
+
+
+                Utils.SetShaderLocation(ref sh, SHADER_LOC_MATRIX_MODEL, "matModel");
+
                 ShaderInit = true;
             }
 
             Utils.SetMaterialShader(ref md, 0, ref sh);
             Utils.MeshTangents(ref md);
-            
 
-            return Tuple.Create(md,sh);
+
+            return Tuple.Create(md, sh);
         }
 
     }
